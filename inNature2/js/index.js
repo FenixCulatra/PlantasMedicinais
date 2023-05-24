88f24d084ba9b93048aa718a6a9e2723e2aa2070
20211TIIMI0095 <joao.scheidegger@gmail.com>
2023-05-24 11:08:27 -0300

    2.5

--- inNature2/js/index.js
+++ inNature2/js/index.js
@@ -1,4 +1,5 @@
 var fotoPerfil = document.getElementById("fotoPerfil");
+var maisPesquisados = document.getElementById("img_maisPesquisado");
 var logado = false;
 var tem_foto = false;
 
@@ -6,4 +7,16 @@ if (logado && tem_foto) {
     alert("Hã?")
 } else {
     fotoPerfil.src = "img/foto_padrao.png";
+}
+
+for (let i = 0; i < 7; i++) {
+    let div = document.createElement("div");
+    let img = document.createElement("img");
+    
+    img.src = "https://fenixculatra.github.io/PlantasMedicinais/imagens/gegibre.jpg";
+    // div.className = "col";
+    div.appendChild(img);
+    
+    // console.log(div);
+    maisPesquisados.appendChild(div);
 }
