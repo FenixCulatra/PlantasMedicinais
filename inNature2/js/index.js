87fb5c30a0ced13f7411e6963ed72fbbcdb0fd25
20211TIIMI0095 <joao.scheidegger@gmail.com>
2023-05-26 11:47:52 -0300

    2

--- inNature2/js/index.js
+++ inNature2/js/index.js
@@ -2,6 +2,19 @@ var fotoPerfil = document.getElementById("fotoPerfil");
 var maisPesquisados = document.getElementById("img_maisPesquisado");
 var logado = false;
 var tem_foto = false;
+var LmP = [
+    {
+        "nome": "Gengibre",
+        "avaliacao": 8,
+        "imagem": "https://fenixculatra.github.io/PlantasMedicinais/imagens/gegibre.jpg"
+    },
+    {
+        "nome": "Capim-Limão",
+        "avaliacao": 7,
+        "imagem": "https://fenixculatra.github.io/PlantasMedicinais/imagens/capim-limao.jpg"
+    }
+];
+
 
 if (logado && tem_foto) {
     alert("Hã?")
@@ -9,13 +22,46 @@ if (logado && tem_foto) {
     fotoPerfil.src = "img/foto_padrao.png";
 }
 
-for (let i = 0; i < 7; i++) {
-    let div = document.createElement("div");
-    let img = document.createElement("img");
+
+
+for (let i = 0; i < LmP.length; i++) {
+    let div         = document.createElement("div");
+    let img         = document.createElement("img");
+    let title       = document.createElement("p");
+    let nota_t      = document.createElement("span");
+    let nota        = LmP[i]["avaliacao"];
+    let nota_r      = Math.ceil(nota/2);
     
-    img.src = "https://fenixculatra.github.io/PlantasMedicinais/imagens/gegibre.jpg";
-    // div.className = "col";
+
+    
+    nota_t.textContent = nota + "/10";
+    nota_t.style.fontSize = "1rem";
+    nota_t.style.marginLeft = "1rem" ;
+
+    console.log(nota);
+
+
+    img.src = LmP[i]["imagem"];
+    title.textContent = LmP[i]["nome"];
+
+    // div.className = "border";
     div.appendChild(img);
+    for (let b = 0; b < 5; b++) {
+        if (nota_r > 0) {
+            nota_r -= 1;
+            let star = document.createElement("span");
+            star.innerHTML = "&#9733";
+            star.style.color = "yellow";
+            div.appendChild(star);
+        } else {
+            let star = document.createElement("span");
+            star.innerHTML = "&#9734";
+            star.style.color = "yellow";
+            div.appendChild(star);
+        }
+    }
+    div.appendChild(nota_t)
+    div.appendChild(title);
     
     // console.log(div);
     maisPesquisados.appendChild(div);
