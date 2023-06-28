8291253e39f3cb1bb63242214ebd5a47461c73f2
João Vitor <91470763+FenixCulatra@users.noreply.github.com>
2023-06-28 10:36:15 -0300

    Update geralJs.js

--- inNature/js/geralJs.js
+++ inNature/js/geralJs.js
@@ -26,7 +26,7 @@ header.innerHTML = `<nav class="navbar d-inline p-0">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="bi bi-search my-auto ms-2" viewBox="0 0 16 16">
                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                 </svg>
-                <input class="form-control border-0 bg-transparent fts-4 w-100 pesquisa" type="search" placeholder="Search" aria-label="Search">
+                <input id="Campode Pesquisa" class="form-control border-0 bg-transparent fts-4 w-100 pesquisa" type="search" placeholder="Search" aria-label="Search">
             </div>
     </div>
     <!-- Fim da Barra de Pesquisa -->
